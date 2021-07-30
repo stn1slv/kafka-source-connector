2cc2bb6dfd6d22761f7012e040556920d24e11f6
Stanislav Deviatov <devyatov@gmail.com>
2021-07-30 09:50:16 +0300

    Send with headers

--- src/main/java/com/acme/kafka/connect/sample/SampleSourceTask.java
+++ src/main/java/com/acme/kafka/connect/sample/SampleSourceTask.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.time.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.connect.header.ConnectHeaders;
 import org.apache.kafka.connect.source.SourceTask;
 
 import static com.acme.kafka.connect.sample.SampleSourceConnectorConfig.*;
@@ -41,14 +43,29 @@ public class SampleSourceTask extends SourceTask {
         List<SourceRecord> records = new ArrayList<>();
         SourceRecord record;
         for (String source : sources) {
-            record = new SourceRecord(Collections.singletonMap("source", source), Collections.singletonMap("offset", 0),
-                    source, null, null, null, Schema.BYTES_SCHEMA, String.format("Data from %s", source).getBytes());
+            record = createSourceRecordWithHeaders(source);
             // log.info("Polling data from the source '" + source + "' " + record.value());
             log.info("Record prepared: " + record.value());
             records.add(record);
         }
         // log.info("Processed " + sources.size() + " source records");
         return records;
+    }
+
+    private SourceRecord createSourceRecord(String source) {
+        Long timestamp = System.currentTimeMillis();
+        return new SourceRecord(Collections.singletonMap("source", source), Collections.singletonMap("offset", 0),
+                source, null, null, null, Schema.BYTES_SCHEMA,
+                String.format("Data from %s [%d]", source, timestamp).getBytes());
+    }
+
+    private SourceRecord createSourceRecordWithHeaders(String source) {
+        Long timestamp = System.currentTimeMillis();
+        ConnectHeaders headers = new ConnectHeaders();
+        headers.addString("X-CUSTOM-HEADER", String.format("Send %d", timestamp));
+        return new SourceRecord(Collections.singletonMap("source", source), Collections.singletonMap("offset", 0),
+                source, null, null, null, Schema.BYTES_SCHEMA,
+                String.format("Data from %s [%d]", source, timestamp).getBytes(), timestamp, headers);
     }
 
     @Override
